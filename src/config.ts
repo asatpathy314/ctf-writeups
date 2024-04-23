725e50597647452f3f2d3fd1eb724d5fcdab4f8d
asatpathy314 <asatpathy314@gmail.com>
2024-04-23 14:24:10 -0400

    fixed link structure

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://asatpathy314.github.io/ctf-writeups", // replace this with your deployed domain
   author: "Abhishek Satpathy",
   desc: "A CTF writeup blog with some extras added in.",
   title: "l33thax0r33",
